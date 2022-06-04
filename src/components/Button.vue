25a059972a3001460d8df57d1128095020d57eac
VishnuPriyan021 <vishnupriyan.balasubaramaniyan@halleyx.com>
2022-06-04 22:51:11 +0530

    Vue Component Project

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+   <button>{{label}}</button>
+</template>
+<script>
+export default {
+    name:'ButtonComponent',
+   data() {
+      return {
+        label: 'Demo'
+      }
+   }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+// import components here
+import ButtonComponent from "./components/Button.vue";
+import AnotherButton from "./components/AnotherButton.vue";
+
+// export components for this library
+export default {
+  ButtonComponent,
+  AnotherButton
+};
